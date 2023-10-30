5a32f7c461b8c705e6af47a739414e18eccf21bf
Kaio Henrique <136716162+Kai141141141@users.noreply.github.com>
2023-10-30 20:12:42 -0300

    Alteração nas condições
    Foram alteradas algumas condições no projeto sendo ela as cores que mostram assim que são gerados o IMC, além de alguns pequenos ajustes como a adição do try e catch.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,14 +31,22 @@
             this.lblPeso = new System.Windows.Forms.Label();
             this.lblAltura = new System.Windows.Forms.Label();
             this.lblRes = new System.Windows.Forms.Label();
+            this.lblClas = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.txbIMC = new System.Windows.Forms.TextBox();
+            this.txbPeso = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.lblCondicao = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblPeso
             // 
             this.lblPeso.AutoSize = true;
-            this.lblPeso.Location = new System.Drawing.Point(12, 58);
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(7, 61);
             this.lblPeso.Name = "lblPeso";
-            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.Size = new System.Drawing.Size(45, 20);
             this.lblPeso.TabIndex = 0;
             this.lblPeso.Text = "Peso";
             this.lblPeso.Click += new System.EventHandler(this.label1_Click);
@@ -46,25 +54,98 @@
             // lblAltura
             // 
             this.lblAltura.AutoSize = true;
-            this.lblAltura.Location = new System.Drawing.Point(12, 96);
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(7, 114);
             this.lblAltura.Name = "lblAltura";
-            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.Size = new System.Drawing.Size(51, 20);
             this.lblAltura.TabIndex = 1;
             this.lblAltura.Text = "Altura";
             // 
             // lblRes
             // 
             this.lblRes.AutoSize = true;
-            this.lblRes.Location = new System.Drawing.Point(46, 160);
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.Location = new System.Drawing.Point(14, 226);
             this.lblRes.Name = "lblRes";
-            this.lblRes.Size = new System.Drawing.Size(0, 13);
+            this.lblRes.Size = new System.Drawing.Size(38, 20);
             this.lblRes.TabIndex = 2;
+            this.lblRes.Text = "IMC";
+            // 
+            // lblClas
+            // 
+            this.lblClas.AutoSize = true;
+            this.lblClas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClas.Location = new System.Drawing.Point(12, 262);
+            this.lblClas.Name = "lblClas";
+            this.lblClas.Size = new System.Drawing.Size(0, 20);
+            this.lblClas.TabIndex = 3;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(41, 19);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(147, 24);
+            this.lblTitulo.TabIndex = 4;
+            this.lblTitulo.Text = "Calcule seu IMC";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(45, 175);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(143, 28);
+            this.btn.TabIndex = 5;
+            this.btn.Text = "CALCULAR";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txbIMC
+            // 
+            this.txbIMC.Enabled = false;
+            this.txbIMC.Location = new System.Drawing.Point(81, 228);
+            this.txbIMC.Name = "txbIMC";
+            this.txbIMC.Size = new System.Drawing.Size(107, 20);
+            this.txbIMC.TabIndex = 8;
+            this.txbIMC.TextChanged += new System.EventHandler(this.txbIMC_TextChanged);
+            // 
+            // txbPeso
+            // 
+            this.txbPeso.Location = new System.Drawing.Point(81, 63);
+            this.txbPeso.Name = "txbPeso";
+            this.txbPeso.Size = new System.Drawing.Size(100, 20);
+            this.txbPeso.TabIndex = 9;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(81, 116);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(100, 20);
+            this.txbAltura.TabIndex = 10;
+            // 
+            // lblCondicao
+            // 
+            this.lblCondicao.AutoSize = true;
+            this.lblCondicao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCondicao.Location = new System.Drawing.Point(7, 273);
+            this.lblCondicao.Name = "lblCondicao";
+            this.lblCondicao.Size = new System.Drawing.Size(0, 20);
+            this.lblCondicao.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(173, 258);
+            this.BackgroundImage = global::CalculadoraIMC.Properties.Resources.Captura_de_tela_2023_10_30_192836;
+            this.ClientSize = new System.Drawing.Size(225, 302);
+            this.Controls.Add(this.lblCondicao);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbPeso);
+            this.Controls.Add(this.txbIMC);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblClas);
             this.Controls.Add(this.lblRes);
             this.Controls.Add(this.lblAltura);
             this.Controls.Add(this.lblPeso);
@@ -80,6 +161,13 @@
         private System.Windows.Forms.Label lblPeso;
         private System.Windows.Forms.Label lblAltura;
         private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label lblClas;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox txbIMC;
+        private System.Windows.Forms.TextBox txbPeso;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Label lblCondicao;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,82 @@ namespace CalculadoraIMC
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                //Colher valores:
+
+                double peso = double.Parse(txbPeso.Text);
+                double haltura = double.Parse(txbAltura.Text);
+
+
+                double res = (peso / Math.Pow(haltura, 2));
+
+                //Especificações das condições:
+
+                if (res <= 18.5)
+                {
+
+                    lblCondicao.Text = "Abaixo do peso";
+                    lblCondicao.ForeColor = Color.LightSalmon;
+                }
+                else if (res >= 18.6 && res <= 24.9)
+                {
+                    lblCondicao.Text = "Peso ideal";
+                    lblCondicao.ForeColor = Color.Green;
+                }
+                else if (res >= 25 && res <= 29.9)
+                {
+                    lblCondicao.Text = "Levimento acima";
+                    lblCondicao.ForeColor = Color.Orange;
+                }
+                else if (res >= 30 && res <= 34.9)
+                {
+                    lblCondicao.Text = "Obsidade grau I";
+                    lblCondicao.ForeColor = Color.DarkOrange;
+                }
+                else if (res >= 35 && res <= 39.9)
+                {
+                    lblCondicao.Text = "Obsidade severa";
+                    lblCondicao.ForeColor = Color.OrangeRed;
+                }
+                else if (res >= 40)
+
+                {
+                    lblCondicao.Text = "Obsidade morbida";
+                    lblCondicao.ForeColor = Color.Red;
+                }
+
+                //Obter resultados:
+
+                txbIMC.Text = Math.Round(res, 2).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Resultado inválido, não ouve dados informados!");
+                //Limpar os dados:
+               txbIMC.Clear();
+               txbPeso.Clear();
+               txbAltura.Clear();
+
+
+            }
+        }
+            
+            
+     
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbIMC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
